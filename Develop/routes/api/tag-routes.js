ba88b4409663fa78fd4d7ef3cd9911437d571971
Daniel Packer <danielapacker@gmail.com>
2022-11-04 22:32:43 -0700

    add first router. function in tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,14 +3,26 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      // be sure to include its associated Product data
+      include: [{model: Product, through: ProductTag}],
+    });
+    console.log(tagData);
+    res.status(200).json(tagData);
+    console.log()
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const categoryData = await Tag.findByPk(req.params.id, {
+      // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
