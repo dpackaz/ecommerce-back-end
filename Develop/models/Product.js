72c80fdf315b3d3c575945162799945ce623bbe9
Daniel Packer <danielapacker@gmail.com>
2022-11-04 21:01:48 -0700

    troubleshoot and finish defining models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -21,17 +21,22 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 10
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'category',
         key: 'id',
