19e23fa6f0815c176a9a9cb1c92e363fe5d83e8a
Daniel Packer <danielapacker@gmail.com>
2022-11-07 11:58:10 -0700

    fix typo

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -67,7 +67,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagDataa = await Tag.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       }
